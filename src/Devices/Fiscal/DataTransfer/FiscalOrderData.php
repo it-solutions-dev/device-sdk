730df2d136b6ed9d25a5744d5404415e97a6be04
Ernestas <ernestas@itsolutions.lt>
2025-04-17 16:38:48 +0300

    Add new field for rounded total

--- src/Devices/Fiscal/DataTransfer/FiscalOrderData.php
+++ src/Devices/Fiscal/DataTransfer/FiscalOrderData.php
@@ -20,21 +20,22 @@ class FiscalOrderData implements FiscalOrderContract
         public ?FiscalPaymentData $transaction = null,
         public ?FiscalPaymentData $points = null,
         public ?array $custom = null,
-    ) {
-    }
+        public ?float $totalAfterRouding = null,
+    ) {}
 
     public static function fill(array $data): self
     {
         return new static(
             total: Arr::get($data, 'total', 0),
             discount: FiscalDiscountData::fill(Arr::get($data, 'discount', [])),
-            products: array_map(fn ($product) => FiscalProductData::fill($product), Arr::get($data, 'products', [])),
+            products: array_map(fn($product) => FiscalProductData::fill($product), Arr::get($data, 'products', [])),
             cash: Arr::get($data, 'cash') ? FiscalPaymentData::fill(Arr::get($data, 'cash')) : null,
             card: Arr::get($data, 'card') ? FiscalPaymentData::fill(Arr::get($data, 'card')) : null,
             coupon: Arr::get($data, 'coupon') ? FiscalPaymentData::fill(Arr::get($data, 'coupon')) : null,
             transaction: Arr::get($data, 'transaction') ? FiscalPaymentData::fill(Arr::get($data, 'transaction')) : null,
             points: Arr::get($data, 'points') ? FiscalPaymentData::fill(Arr::get($data, 'points')) : null,
             custom: Arr::get($data, 'custom', null),
+            totalAfterRouding: Arr::get($data, 'total_after_rouding', null),
         );
     }
 
@@ -43,13 +44,14 @@ class FiscalOrderData implements FiscalOrderContract
         return [
             'total' => $this->total,
             'discount' => $this->discount->toArray(),
-            'products' => array_map(fn ($product) => $product->toArray(), $this->products),
+            'products' => array_map(fn($product) => $product->toArray(), $this->products),
             'cash' => $this->cash?->toArray(),
             'card' => $this->card?->toArray(),
             'coupon' => $this->coupon?->toArray(),
             'transaction' => $this->transaction?->toArray(),
             'points' => $this->points?->toArray(),
             'custom' => $this->custom,
+            'total_after_rouding' => $this->totalAfterRouding,
         ];
     }
 }
--- src/Devices/Fiscal/DataTransfer/FiscalReturnData.php
+++ src/Devices/Fiscal/DataTransfer/FiscalReturnData.php
@@ -17,20 +17,21 @@ class FiscalReturnData implements FiscalReturnContract
         public ?FiscalPaymentData $coupon = null,
         public ?FiscalPaymentData $transaction = null,
         public ?array $custom = null,
-    ) {
-    }
+        public ?float $totalAfterRouding = null,
+    ) {}
 
     public static function fill(array $data): self
     {
         return new static(
 
             total: Arr::get($data, 'total', 0),
-            products: array_map(fn ($product) => FiscalProductData::fill($product), Arr::get($data, 'products', [])),
+            products: array_map(fn($product) => FiscalProductData::fill($product), Arr::get($data, 'products', [])),
             cash: Arr::get($data, 'cash') ? FiscalPaymentData::fill(Arr::get($data, 'cash')) : null,
             card: Arr::get($data, 'card') ? FiscalPaymentData::fill(Arr::get($data, 'card')) : null,
             coupon: Arr::get($data, 'coupon') ? FiscalPaymentData::fill(Arr::get($data, 'coupon')) : null,
             transaction: Arr::get($data, 'transaction') ? FiscalPaymentData::fill(Arr::get($data, 'transaction')) : null,
             custom: Arr::get($data, 'custom', null),
+            totalAfterRouding: Arr::get($data, 'total_after_rouding', null),
         );
     }
 
@@ -38,12 +39,13 @@ class FiscalReturnData implements FiscalReturnContract
     {
         return [
             'total' => $this->total,
-            'products' => array_map(fn ($product) => $product->toArray(), $this->products),
+            'products' => array_map(fn($product) => $product->toArray(), $this->products),
             'cash' => $this->cash?->toArray(),
             'card' => $this->card?->toArray(),
             'coupon' => $this->coupon?->toArray(),
             'transaction' => $this->transaction?->toArray(),
             'custom' => $this->custom,
+            'total_after_rouding' => $this->totalAfterRouding,
         ];
     }
 }
