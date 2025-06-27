f0dfc0cdbd48e338c8599576d9a073cb26b1660e
Ernestas <ernestas@itsolutions.lt>
2025-06-27 09:35:56 +0300

    Class param fix

--- src/Devices/Printer/DataTransfer/OrderReceipt.php
+++ src/Devices/Printer/DataTransfer/OrderReceipt.php
@@ -9,22 +9,21 @@ class OrderReceipt extends Data
 {
     public function __construct(
         public string $device,
-        public string $datatime, // Y-m-d H:i:s
-        public string $timezone = 'UTC',
         public string $receiptNumber,
+        public string $datatime, // Y-m-d H:i:s
+        public string $timezone,
         #[DataCollectionOf(PrintLine::class)]
         public array $companyDetails,
         #[DataCollectionOf(OrderReceiptItem::class)]
         public array $items,
-        public float $total = 0,
         #[DataCollectionOf(OrderReceiptVat::class)]
         public array $vats,
+        public float $total = 0,
         public ?float $totalAfterRounding = 0,
         public ?bool $rounddbl = false,
         public ?OrderReceiptPayment $cash = null,
         public ?OrderReceiptPayment $card = null,
         public ?OrderReceiptPayment $coupon = null,
         public ?OrderReceiptPayment $transaction = null,
-        // public ?array $custom = null,
     ) {}
 }
