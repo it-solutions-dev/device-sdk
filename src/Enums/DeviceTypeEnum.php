b0e4decc915e9f1aa9aa614e0c5060b60f028c2e
Ernestas <ernestas@itsolutions.lt>
2025-11-06 11:34:10 +0200

    Added kiosk and pos as device types

--- src/Enums/DeviceTypeEnum.php
+++ src/Enums/DeviceTypeEnum.php
@@ -4,6 +4,8 @@ namespace Its\DeviceSdk\Enums;
 
 enum DeviceTypeEnum: string
 {
+    case Pos = 'pos';
+    case Kiosk = 'kiosk';
     case Printer = 'printer';
     case Scanner = 'scanner';
     case Fiscal = 'fiscal';
